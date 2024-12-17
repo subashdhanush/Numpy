2afb3777f4cb33abe7b39d02dfbc4e75c0a3931b
subashdhanush <85820233+subashdhanush@users.noreply.github.com>
2024-12-17 16:02:21 +0530

    Add files via upload

---
+++ thirdpgm.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+np1=np.array([-3,-2,-1,0,1,2,3,4,5,6,7,8,9])
+print(np1)
+
+#square root of each element
+print(np.sqrt(np1))
+
+#Absolute value for each element
+print(np.absolute(np1))
+
+#exponents value for each element
+print(np.exp(np1))
+
+#Min/Max
+print(np.min(np1))
+print(np.max(np1))
+
+#sign positive or negative
+print(np.sign(np1))
+
+#log
+print(np.log(np1))
